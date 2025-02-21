19f9febbf1e4bd9365433141d2f873792a0fcf93
THROATPIES <TS.Shinigami@proton.me>
2025-02-20 21:40:23 -0800

    Update CURRENT_EPISODE to 7 and adjust VIDEO_FONT_SIZE to 48; add MAX_VIDEO_DURATION constant

--- src/constants.rs
+++ src/constants.rs
@@ -1,11 +1,13 @@
-pub const CURRENT_EPISODE: i32 = 6;
+pub const CURRENT_EPISODE: i32 = 7;
 pub const OUTPUTS_FOLDER: &str = "data/output";
 pub const AUDIO_OUTPUT_PATH: &str = "data/output/output.wav";
 pub const VIDEO_INPUT_PATH: &str = "data/inputs/input.mp4";
+// pub const VIDEO_INPUT_PATH: &str = "data/inputs/input_2.mp4";
 pub const VIDEO_OUTPUT_PATH: &str = "data/output/output.mp4";
 pub const AUDIO_VOICE: Option<&str> = Some("af_bella");
 pub const AUDIO_MODEL: Option<&str> = Some("a");
-pub const VIDEO_FONT_SIZE: Option<i32> = Some(76);
+pub const VIDEO_FONT_SIZE: Option<i32> = Some(48);
 pub const VIDEO_BG_COLOR: Option<&str> = Some("black");
 pub const UPLOAD_CATEGORY: &str = "22";
 pub const UPLOAD_PRIVACY: &str = "public";
+pub const MAX_VIDEO_DURATION: f32 = 60.0; // Maximum duration in seconds
